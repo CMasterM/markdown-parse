aadaefe525693f15a51f1bca61649819eb95a1df
CMasterM <97553498+CMasterM@users.noreply.github.com>
2022-02-03 13:30:31 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,6 +12,7 @@ public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+        assertEquals(3, 1+1);
     }
 
     public String readFile(String file) throws IOException {
